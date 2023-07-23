ca30bdc34e39f242d75bd08d0b866c5dab4e818f
Owen Lindsey <138630308+Omni-v@users.noreply.github.com>
2023-07-22 19:26:47 -0700

    ACTIVITY 6 UPDATES

---
+++ TemperatureConversion/TemperatureConversion.Designer.cs
@@ -0,0 +1,32 @@
+namespace TemperatureConversion
+{
+    partial class temperatureConversionApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        ///private System.ComponentModel.IContainer components = null;
+        ///                      
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        ///  protected override void Dispose(bool disposing)
+        ///  {
+        ///  / Clean up any resources being used
+        ///   if (disposing && (components != null))
+        /// {
+        /// components.Dispose();
+        /// }
+        ///  base.Dispose(disposing);
+        ///   }
+        ///                                                                                                                 
+        private Label inputInstrucitons;
+        private TextBox FahrenheitInput;
+        private Label CelciusOutputInformation;
+        private TextBox celciusInformation;
+        private Button FahrenheitToCelcius;
+        private TextBox textBox1;
+        private Button resetCalculation;
+    }
+}
