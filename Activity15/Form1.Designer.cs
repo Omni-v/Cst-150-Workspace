e2dfd59a6a0488b3e0f890359a68d76874d9b430
Owen Lindsey <138630308+Omni-v@users.noreply.github.com>
2023-08-20 19:01:47 -0700

    Activity15

---
+++ Activity15/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Activity15
+{
+    public partial class LuckyNumberForm : Form
+    {
+        public LuckyNumberForm()
+        {
+            InitializeComponent();
+            PopulateYears();
+            goButton.Click += GoButton_Click;
+        }
+
+        private void PopulateYears()
+        {
+            for (int i = DateTime.Now.Year; i >= 1900; i--)
+            {
+                cbxYears.Items.Add(i);
+            }
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            if (cbxYears.SelectedItem != null)
+            {
+                int selectedYear = int.Parse(cbxYears.SelectedItem.ToString());
+                Form2 form2 = new Form2(selectedYear);
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please select a year before proceeding.");
+            }
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            cbxYears = new ComboBox();
+            goButton = new Button();
+            txtPrompt = new TextBox();
+
+            SuspendLayout();
+
+            // cbxYears
+            cbxYears.FormattingEnabled = true;
+            cbxYears.Location = new Point(95, 92);
+            cbxYears.Name = "cbxYears";
+            cbxYears.Size = new Size(121, 27);
+            cbxYears.TabIndex = 0;
+
+            // goButton
+            goButton.Location = new Point(118, 125);
+            goButton.Name = "goButton";
+            goButton.Size = new Size(75, 23);
+            goButton.TabIndex = 1;
+            goButton.Text = "Go";
+            goButton.UseVisualStyleBackColor = true;
+
+            // txtPrompt
+            txtPrompt.Location = new Point(4, 60);
+            txtPrompt.Name = "txtPrompt";
+            txtPrompt.Size = new Size(299, 26);
+            txtPrompt.TabIndex = 2;
+            txtPrompt.Text = "select a year to recieve a random number. ";
+            txtPrompt.TextAlign = HorizontalAlignment.Center;
+
+            // LuckyNumberForm
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGoldenrod;
+            ClientSize = new Size(309, 259);
+            Controls.Add(txtPrompt);
+            Controls.Add(goButton);
+            Controls.Add(cbxYears);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "LuckyNumberForm";
+            Text = "Random Number Generator";
+
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cbxYears;
+        private Button goButton;
+        private TextBox txtPrompt;
+    }
+}
---
+++ Activity15/Form2.Designer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Activity15
+{
+    public partial class Form2 : Form
+    {
+        private Label lblMessage;
+        private Label lblLuckyNumber;
+
+        public Form2(int year)
+        {
+            InitializeComponent();
+            CalculateLuckyNumber(year);
+        }
+
+        private void CalculateLuckyNumber(int year)
+        {
+            int luckyNumber = (year * new Random().Next(1, 11)) % 100;
+            lblMessage.Text = $"Your lucky number for the year {year} is:";
+            lblLuckyNumber.Text = luckyNumber.ToString();
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            lblMessage = new Label();
+            lblLuckyNumber = new Label();
+            SuspendLayout();
+
+            // lblMessage
+            lblMessage.AutoSize = true;
+            lblMessage.Location = new Point(90, 58);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(0, 17);
+            lblMessage.TabIndex = 0;
+            lblMessage.TextAlign = ContentAlignment.BottomCenter;
+
+            // lblLuckyNumber
+            lblLuckyNumber.AutoSize = true;
+            lblLuckyNumber.Location = new Point(160, 128);
+            lblLuckyNumber.Name = "lblLuckyNumber";
+            lblLuckyNumber.Size = new Size(0, 17);
+            lblLuckyNumber.TabIndex = 1;
+
+            // Form2
+            AutoScaleDimensions = new SizeF(9F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Coral;
+            ClientSize = new Size(372, 216);
+            Controls.Add(lblLuckyNumber);
+            Controls.Add(lblMessage);
+            Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form2";
+            Text = "Your Lucky Number";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+    }
+}
